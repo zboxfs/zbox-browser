da8dd8b9ebb137897802e7ac6b396b12cd3b76cf
Bo Lu <lv.patrick@gmail.com>
2019-01-27 15:41:30 +1100

    update build script

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@ set -ex
 
 docker run \
     --rm \
-    --volume $PWD:/root/zbox \
+    --volume $PWD/zbox:/root/zbox \
     zboxfs/wasm \
     cargo build --target wasm32-unknown-unknown --features storage-zbox-wasm --release
 
 docker run \
     --rm \
-    --volume $PWD:/root/zbox \
+    --volume $PWD/zbox:/root/zbox \
+    --volume $PWD/src/wasm:/output \
     zboxfs/wasm \
-    wasm-bindgen target/wasm32-unknown-unknown/release/zbox.wasm --out-dir ./wasm/src/wasm
+    wasm-bindgen target/wasm32-unknown-unknown/release/zbox.wasm --out-dir /output
